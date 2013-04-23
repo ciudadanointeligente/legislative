db41e2e0aa10fd87aaca212de7658d5058473881
J. Pablo Pérez Trabucco <jpereztrabucco@gmail.com>
2013-04-23 14:36:06 -0300

    Changes basic search; closes #1

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -5,7 +5,6 @@ class BillsController < ApplicationController
   include Roar::Rails::ControllerAdditions
 
   def index
-    @bills = Bills.get("http://localhost:9292/bills", 'application/json').bills
   end
 
   def edit
@@ -13,24 +12,10 @@ class BillsController < ApplicationController
   end
 
   def search
-    puts '<params>'
-    puts params
-    puts '</params>'
-
     if !params.nil? && params.length > 2  # default have 2 keys {"action"=>"search", "controller"=>"bills"}
-      @bills = Bills.get("http://localhost:9292/search/origin_chamber=#{params['origin_chamber']}", 'application/json').bills || []
-
-      # @bills = Bills.get 'http://localhost:9292/search', 'application/json' do |request|
-        # request.headers['Content-Type'] = 'application/json'
-        # request.body = params.to_json
-        # request.body = {origin_chamber: params['origin_chamber']}.to_json
-        # puts '<origin_chamber>'
-        # puts params['origin_chamber']
-        # puts '</origin_chamber>'
-      # end.bills
-
+      @bills = Bills.get("http://billit.ciudadanointeligente.org/bills/search?q=#{params[:q]}", 'application/json').bills || []
     else
-      @bills = Bills.get("http://localhost:9292/search", 'application/json').bills
+      # @bills = nil
     end
   end
 
