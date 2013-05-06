45eb73e5ccc54da65a1db9fef6ffc7ced7db95d5
J. Pablo Pérez Trabucco <jpereztrabucco@gmail.com>
2013-05-06 11:52:02 -0400

    Syntax changes

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -1,6 +1,6 @@
 #encoding: utf-8
-require "billit_representers/representers/bill_representer"
-require "billit_representers/representers/bills_representer"
+require 'billit_representers/representers/bill_representer'
+require 'billit_representers/representers/bills_representer'
 
 class BillsController < ApplicationController
   include Roar::Rails::ControllerAdditions
@@ -13,22 +13,22 @@ class BillsController < ApplicationController
   end
 
   def advanced_search
-    if !params.nil? && params.length > 2 # default have 2 keys {"action"=>"advanced_search", "controller"=>"bills"}
+    if !params.nil? && params.length > 2 # default have 2 keys {'action'=>'advanced_search', 'controller'=>'bills'}
       keywords = String.new
       params.each do |param|
         if param[0] != 'utf8' && param[0] != 'commit'
          keywords << param[0] + '=' + param[1] + '&'
         end
       end
-      @bills = Bills.get("http://billit.ciudadanointeligente.org/bills/search/?#{URI.encode(keywords)}", 'application/json').bills || []
+      @bills = Bills.get('http://billit.ciudadanointeligente.org/bills/search/?#{URI.encode(keywords)}', 'application/json').bills || []
     else
       # @bills = nil
     end
   end
 
   def search
-    if !params.nil? && params.length > 2  # default have 2 keys {"action"=>"search", "controller"=>"bills"}
-      @bills = Bills.get("http://billit.ciudadanointeligente.org/bills/search?q=#{URI.encode(params[:q])}", 'application/json').bills || []
+    if !params.nil? && params.length > 2  # default have 2 keys {'action'=>'search', 'controller'=>'bills'}
+      @bills = Bills.get('http://billit.ciudadanointeligente.org/bills/search?q=#{URI.encode(params[:q])}', 'application/json').bills || []
     else
       # @bills = nil
     end
