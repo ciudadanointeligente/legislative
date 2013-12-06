3d3fbd5e6d848c1a178d494a2456509f4fd8e4d4
José Damián Garrido Muñoz <damian.garrido.munoz@gmail.com>
2013-12-06 16:52:20 -0300

    display tables display bills enabled search in home

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,9 @@
 class MainController < ApplicationController
   def index
+    @tables = TableCollection.new
+    @tables.get(ENV['tables'],'application/json')
+
+    @bills = BillCollection.new
+    @bills.get(ENV['billit']+'search.json?per_page=6', 'application/json')
   end
 end
