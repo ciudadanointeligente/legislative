b594371b330a5cb0ad09770e42de159d50a4fcf4
jgarrido <jgarrido@debian>
2013-12-23 12:04:27 -0300

    add two options in votations visualization, ausent and pairs fix javascript add classes for ausents and pairs

--- app/assets/javascripts/mainmain.js
+++ app/assets/javascripts/mainmain.js
@@ -13,31 +13,72 @@ $(document).ready(function(){
     container: "tags-tooltip"
   });
 
+  var backgroundColor = 'background-color:#FFF; opacity:0.8;'
+
   $('.green-votes-bar').mouseover(function(e){
     $('span.positive').removeAttr('style','');
-    $('span.against').attr('style','background-color:#FFF; opacity:0.8;');
-    $('span.abstention').attr('style','background-color:#FFF; opacity:0.8;');
+    $('span.against').attr('style',backgroundColor);
+    $('span.abstention').attr('style',backgroundColor);
+    $('span.ausent').attr('style',backgroundColor);
+    $('span.paring').attr('style',backgroundColor);
   }).mouseout(function(){
     $('span.against').removeAttr('style','');
     $('span.abstention').removeAttr('style','');
+    $('span.paring').removeAttr('style','');
+    $('span.ausent').removeAttr('style','');
   });
 
   $('.red-votes-bar').mouseover(function(e){
     $('span.against').removeAttr('style','');
-    $('span.positive').attr('style','background-color:#FFF; opacity:0.8;');
-    $('span.abstention').attr('style','background-color:#FFF; opacity:0.8;');
+    $('span.positive').attr('style',backgroundColor);
+    $('span.abstention').attr('style',backgroundColor);
+    $('span.ausent').attr('style',backgroundColor);
+    $('span.paring').attr('style',backgroundColor);
   }).mouseout(function(){
     $('span.positive').removeAttr('style','');
     $('span.abstention').removeAttr('style','');
+    $('span.paring').removeAttr('style','');
+    $('span.ausent').removeAttr('style','');
   });
 
-  $('.blue-votes-bar').mouseover(function(e){
+  $('.skyblue-votes-bar').mouseover(function(e){
     $('span.abstention').removeAttr('style','');
-    $('span.against').attr('style','background-color:#FFF; opacity:0.8;');
-    $('span.positive').attr('style','background-color:#FFF; opacity:0.8;');
+    $('span.against').attr('style',backgroundColor);
+    $('span.positive').attr('style',backgroundColor);
+    $('span.ausent').attr('style',backgroundColor);
+    $('span.paring').attr('style',backgroundColor);
   }).mouseout(function(){
     $('span.positive').removeAttr('style','');
     $('span.against').removeAttr('style','');
+    $('span.abstention').removeAttr('style','');
+    $('span.paring').removeAttr('style','');
+  });
+
+  $('.yellow-votes-bar').mouseover(function(e){
+    $('span.ausent').removeAttr('style','');
+    $('span.against').attr('style',backgroundColor);
+    $('span.positive').attr('style',backgroundColor);
+    $('span.abstention').attr('style',backgroundColor);
+    $('span.paring').attr('style',backgroundColor);
+  }).mouseout(function(){
+    $('span.positive').removeAttr('style','');
+    $('span.against').removeAttr('style','');
+    $('span.abstention').removeAttr('style','');
+    $('span.paring').removeAttr('style','');
+    $('span.ausent').removeAttr('style','');
+  });
+
+  $('.blue-votes-bar').mouseover(function(e){
+    $('span.paring').removeAttr('style','');
+    $('span.against').attr('style',backgroundColor);
+    $('span.positive').attr('style',backgroundColor);
+    $('span.abstention').attr('style',backgroundColor);
+    $('span.ausent').attr('style',backgroundColor);
+  }).mouseout(function(){
+    $('span.positive').removeAttr('style','');
+    $('span.against').removeAttr('style','');
+    $('span.abstention').removeAttr('style','');
+    $('span.ausent').removeAttr('style','');
   });
 
 });
