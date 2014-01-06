8226e31df3d8c55d785abc98afaff6db0ead2e3b
jdgarrido <damian.garrido.munoz@gmail.com>
2014-01-06 16:35:24 -0300

    comment lines limit to 3 results change matters name add links to tables create a loop for bills in search add var for loops of bills

--- app/controllers/mains_controller.rb
+++ app/controllers/mains_controller.rb
@@ -1,9 +1,9 @@
 class MainsController < ApplicationController
   def index
-    @tables = TableCollection.new
-    @tables.get(ENV['tables'],'application/json')
+    #@tables = TableCollection.new
+    #@tables.get(ENV['tables'],'application/json')
 
-    @bills = BillCollection.new
-    @bills.get(ENV['billit']+'search.json?per_page=6', 'application/json')
+    #@bills = BillCollection.new
+    #@bills.get(ENV['billit']+'search.json?per_page=6', 'application/json')
   end
 end
