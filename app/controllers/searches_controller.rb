97541a3021829563bbbbeb4e743f12ecccc993f6
jdgarrido <damian.garrido.munoz@gmail.com>
2014-02-03 16:06:05 -0300

    refactor search

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,15 +3,6 @@ require 'httparty'
 
 class SearchesController < ApplicationController
   def index
-    response = HTTParty.get(ENV['popit_persons'])
-    json_response = JSON.parse(response.body)
-    authors_detail_list = json_response['result']
-
-    @authors_list = []
-    @persons_query = []
-    authors_detail_list.map do |author|
-      @authors_list.push(author['name'])
-    end
 
     if !params.nil? && params.length > 3 # default have 3 keys {'action'=>'index', 'controller'=>'searchs', "locale"=>"xx"}
       
@@ -31,20 +22,14 @@ class SearchesController < ApplicationController
          @keywords << param[0] + '=' + param[1] + '&'
         end
       end
-      if params['authors'] != nil
-        @authors_list = []
-        authors_detail_list.map do |author|
-          if params['authors'] == author['name']
-            @persons_query.push(author)
-          end
-        end
-      end
+
       @bills_query = BillCollectionPage.get(ENV['billit_url'] + "search.json/?#{URI.encode(@keywords)}&per_page=3", 'application/json')
     else
       @bills_query = BillCollectionPage.get(ENV['billit_url'] + "search.json/?per_page=3", 'application/json')
     end
     
     @parliamentarians = PopitPersonCollection.new
-    @parliamentarians.get ENV['popit_persons'], 'application/json'
+    @parliamentarians.get ENV['popit_search'] + "#{URI.encode(@keywords)}per_page=3", 'application/json'
+    
   end
 end
