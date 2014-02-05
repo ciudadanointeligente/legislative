1a7de2f48254e17d38845134ba1f259d1094a89c
jdgarrido <damian.garrido.munoz@gmail.com>
2014-02-05 16:31:39 -0300

    upgrade views parliamentarians
    remove links to suscriptions and questions
    remove duplicated information
    add Party and Representation Place
    roll back to dummie information of a parliamentarian
    add a dummie image in case of dont have one

--- app/controllers/parliamentarians_controller.rb
+++ app/controllers/parliamentarians_controller.rb
@@ -8,7 +8,11 @@ class ParliamentariansController < ApplicationController
 
   # GET /parliamentarians/1
   def show
-    #@parliamentarian = PopitPerson.get(ENV['popit_persons'] + "#{params[:id]}", 'application/json')
+    #@parliamentarians = PopitPersonCollection.new
+    #@parliamentarians.get ENV['popit_persons'], 'application/json'
+    #@parliamentarians.persons.each do | parliamentarian |
+    #  @parliamentarian = parliamentarian if parliamentarian.id == params[:id]
+    #end
     @parliamentarian = PopitPerson.new
     @parliamentarian.name = 'Pedro Pablo Alvarez-Salamanca Ramirez'
     @parliamentarian.links = JSON.parse ('[{"url": "www.arenasdiputado.cl/","note": "Website"},{"url": "http://www.camara.cl/pdf.aspx?prmid=507&prmtipo=TRANSPARENCIA","note": "Declaration Interes"},{"url": "http://www.camara.cl/pdf.aspx?prmid=392&prmtipo=TRANSPARENCIA","note": "Declaration Patrimonio"},{"url": "http://facebook.com/group.php?gid=103267507483","note": "Facebook"}]')
