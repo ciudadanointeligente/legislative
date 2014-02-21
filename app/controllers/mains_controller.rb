27ff6418a6ffcb6c93e40d842c6c9d58798f57fc
jdgarrido <damian.garrido.munoz@gmail.com>
2014-02-21 14:33:41 -0300

    remove commented lines

--- app/controllers/mains_controller.rb
+++ app/controllers/mains_controller.rb
@@ -2,11 +2,5 @@ class MainsController < ApplicationController
   def index
     @condition_search = true
     @condition_priority_box = true
-    
-    #@tables = TableCollection.new
-    #@tables.get(ENV['tables_url'],'application/json')
-
-    #@bills = BillCollection.new
-    #@bills.get(ENV['billit_url']+'search.json?per_page=6', 'application/json')
   end
 end
