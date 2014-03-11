01791b2e511b0c042a7e3e31b52b7735b16bf81e
jdgarrido <damian.garrido.munoz@gmail.com>
2014-03-11 11:38:05 -0300

    bugfix

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -2,19 +2,22 @@ require 'billit_representers/models/bill'
 
 class Billit::Bill
   def icon
-    case self.status.strip
-    when "Archivado"
-      return 'filed.png'
-    when "Publicado"
-      return 'published.png'
-    when "En tramitación"
-      return 'paperwork.png' #icono pendiente 
-    when "Rechazado"
-      return 'rejected.png' 
-    when "Retirado"
-      return 'discarded.png' 
-    else
-      return ''
+    if ! self.status.blank?
+      case self.status.strip
+      when "Archivado"
+        return 'filed.png'
+      when "Publicado"
+        return 'published.png'
+      when "En tramitación"
+        return 'paperwork.png' #icono pendiente 
+      when "Rechazado"
+        return 'rejected.png' 
+      when "Retirado"
+        return 'discarded.png' 
+      else
+        return 'paperwork.png'
+      end
     end
+    return 'paperwork.png'
   end
 end
