2d5c08d51319e8f63229c2feb17322edfdd5439d
J. Pablo Pérez Trabucco <jpereztrabucco@gmail.com>
2014-03-12 14:33:40 -0300

    Renamed main javascript asset

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,3 +1,6 @@
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
+
 $(document).ready(function(){
   // set up hover panels
   // although this can be done without JavaScript, we've attached these events
