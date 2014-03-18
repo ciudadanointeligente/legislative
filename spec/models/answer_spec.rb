0d1d97f0e10cc9e3b3b275d1e4075cb433a6a38f
Felipe Álvarez <falvarez@votainteligente.cl>
2014-03-18 13:17:32 -0300

    Answers belong to a message

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -12,3 +12,18 @@ describe LegislativeAnswerRepresenter do
 		answer.id.should eql 1
 	end
 end
+
+describe LegislativeAnswerCollection do
+  it "gets the messages from the server" do
+    value = %x( ./writeit_for_testing/writeit_install_yaml.bash example_with_one_message.yaml )
+    collection = LegislativeAnswerCollection.new
+    collection.get
+
+    collection.objects.length.should eql 1
+    answer = collection.objects[0]
+    answer.should be_an_instance_of LegislativeAnswerRepresenter
+    answer.content.should eql 'Hello I am an answer'
+    answer.created.should eql "2014-03-17T00:00:00"
+    answer.id.should eql 1
+  end
+end
