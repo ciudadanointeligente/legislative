5f234389cb2bbbd7788459f3c1b5ca0b6988b107
Felipe Álvarez <falvarez@votainteligente.cl>
2014-04-10 18:29:44 -0300

    decoupling mocks

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,6 +8,7 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
 require 'webmock/rspec'
+require 'mocks'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
