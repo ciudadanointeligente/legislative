d05f07893b821795212b4a410e40b2bfcac5fd30
José Damián Garrido Muñoz <damian.garrido.munoz@gmail.com>
2014-05-16 09:35:53 -0400

    add env locale

--- config/application.rb
+++ config/application.rb
@@ -38,7 +38,7 @@ module Legislative
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.default_locale = :es
+    config.i18n.default_locale = ENV['default_locale']
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
