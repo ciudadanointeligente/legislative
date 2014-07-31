bc0f13256a47c13ad4b446c2839efd01d6d711a4
J. Pablo Pérez Trabucco <jpereztrabucco@gmail.com>
2014-07-31 17:38:02 -0400

    added congressman picture to the answers in communications view

--- app/helpers/communications_helper.rb
+++ app/helpers/communications_helper.rb
@@ -1,2 +1,33 @@
 module CommunicationsHelper
+	def congressmen_profile(name)
+	  query = sprintf('select * from data where name = "%s" limit 1', I18n.transliterate(name))
+	  query = URI::escape(query)
+	  response = RestClient.get(ENV['congressmen_helper_url'] + query, :content_type => :json, :accept => :json, :"x-api-key" => ENV['morph_io_api_key'])
+	  response = JSON.parse(response).first
+	  if !response.nil?
+	    return {'id' => response['uid'], 'name' => name, 'image' => response['profile_image']}
+	  else
+	    return {'id' => 0, 'name' => name, 'image' => nil}
+	  end
+	end
+
+	def congressmen_pic(name, img)
+	  fantasy_name = Rack::Utils.escape(I18n.transliterate(name.gsub("'", "")))
+	  url_image = open('app/assets/images/default-profile.png')
+
+	  if !File.exist?('app/assets/images/congressman/'+fantasy_name+'.jpg')
+	    image = Magick::ImageList.new
+	    begin
+	      Timeout.timeout(5) do
+	        url_image = open(img)
+	      end
+	    rescue Timeout::Error
+	      url_image = open('app/assets/images/default-profile.png')
+	    end
+	    image.from_blob(url_image.read)
+	    crop = image.crop(ENV['congressman_pic_x'].to_i,ENV['congressman_pic_y'].to_i,ENV['congressman_pic_w'].to_i,ENV['congressman_pic_h'].to_i)
+	    crop.write('app/assets/images/congressman/'+fantasy_name+'.jpg')
+	  end
+	  return fantasy_name
+	end
 end
