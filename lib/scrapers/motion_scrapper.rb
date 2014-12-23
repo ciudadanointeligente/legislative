4a23bf029f76f1476ca5452fd66a0b03c2d8f9d5
Felipe Álvarez <falvarez@votainteligente.cl>
2014-12-23 17:31:04 -0300

    Getting motions

--- lib/scrapers/motion_scrapper.rb
+++ lib/scrapers/motion_scrapper.rb
@@ -1,15 +1,39 @@
 require 'popolo'
+require 'nokogiri'
+require 'htmlentities'
 
 module CongresoAbiertoScrapers
-	class MotionScrapingRunRecords
+	class MotionScrapingRunRecord
 		include Mongoid::Document
 		field :date, type: Date
 	end
 
 	class MotionScraper < Pupa::Processor
 		def scrape_motion
-			record = MotionScrapingRunRecords.new date:Date.today
+			record = MotionScrapingRunRecord.new date:Date.today
 			record.save()
+			url = 'http://www.senado.cl/wspublico/tramitacion.php?fecha=12%2F12%2F2014'
+			doc = open(url).read
+			xml_doc  = Nokogiri::XML doc
+			xml_doc.css('proyecto').each do |proyecto_|
+
+				proyecto_.css('votaciones').each do |votaciones|
+					votaciones.css('votacion').each do |votacion|
+						motion = Popolo::Motion.new
+						motion.date = Date.strptime proyecto_.css('descripcion fecha_ingreso').first.content, '%d/%m/%Y'
+						motion.text = proyecto_.css('descripcion titulo').first.content
+						motion.save()
+						votacion.css('VOTO').each do |voto|
+							parlamentario_name = voto.css('PARLAMENTARIO').first.content 
+							parlamentario_seleccion = voto.css('SELECCION').first.content
+						end
+					end
+				end
+			end
+			# xml_doc.xpath('//proyectos/proyecto').each do |proyecto|
+			# 	puts proyecto.xpath['boletin/text()']
+			# end
+
 		end
 	end
 end
--- spec/pupa_scrapers/motion_scraper_spec.rb
+++ spec/pupa_scrapers/motion_scraper_spec.rb
@@ -10,7 +10,9 @@ WebMock.disable_net_connect!(:allow => /popoloproject.com/)
 
 describe CongresoAbiertoScrapers::MotionScraper , "The Motion Scrapper" do
 	before :each do
-		CongresoAbiertoScrapers::MotionScrapingRunRecords.all.delete()
+		CongresoAbiertoScrapers::MotionScrapingRunRecord.all.delete()
+		$file = File.read('./spec/fixtures/ultimas_tramitaciones.xml')
+		stub_request(:get, "http://www.senado.cl/wspublico/tramitacion.php?fecha=12/12/2014").to_return(:body => $file)
 	end
 	context "it self" do
 		it "initializes" do
@@ -27,12 +29,44 @@ describe CongresoAbiertoScrapers::MotionScraper , "The Motion Scrapper" do
 		it "records every run" do
 			runner = Pupa::Runner.new(CongresoAbiertoScrapers::MotionScraper)
 			runner.run([])
-			records = CongresoAbiertoScrapers::MotionScrapingRunRecords.all()
+			records = CongresoAbiertoScrapers::MotionScrapingRunRecord.all()
 			expect(records.count).to eq(1)
 			record = records.first
 			expect(record.date).to eq(today)
 
 
 		end
+	end
+	context "there are two existing record" do
+		before :each do
+			
+		end
+		before :all do
+			CongresoAbiertoScrapers::MotionScrapingRunRecord.new date:Date.strptime('2014-11-12')
+			CongresoAbiertoScrapers::MotionScrapingRunRecord.new date:Date.strptime('2014-12-12')
+
+
+		end
+		it "creates motions" do
+			runner = Pupa::Runner.new(CongresoAbiertoScrapers::MotionScraper)
+			runner.run([])
+			motions = Popolo::Motion.where(
+				date:Date.strptime('2007-11-20'))
+			expect(motions.count).to  be > 0
+			motion = motions.first
+			expect(motion.text).to eq('Rechazo letra a) Indicación N°62  , Partida 10 Ministerio de Justicia (Boletín N°8.575-05) Proyecto de Ley de Presupuestos.')
+			
+
+		end
+		# it "creates one specific one" do
+		# 	runner = Pupa::Runner.new(CongresoAbiertoScrapers::MotionScraper)
+		# 	runner.run([])
+		# 	motions = Popolo::Motion.where(
+		# 		text: "Votación para volver a Comisión el proyecto de ley, en segundo trámite constitucional, que fija porcentajes mínimos de emisión de música nacional y música de raíz folklórica oral a la radiodifusión chilena, para un nuevo primer informe de la Comisión de Educación, Cultura, Ciencia y Tecnología. (Boletín N° 5.491-24)."
+		# 		)
+		# 	expect(motions.count).to eq(1)
+		# 	motion = motions.first
+
+		# end
 	end
 end
