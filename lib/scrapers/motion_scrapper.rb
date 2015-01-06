c9c186d07d82a52bd648cfbc9aa775c88a8ed643
Felipe Álvarez <falvarez@votainteligente.cl>
2015-01-06 15:23:58 -0300

    Escrapeando votos de los diputados ================================== Estamos haciendo el siguiente razonamiento lógico:
    Vamos a buscar al servicio [Listado de proyectos que han tenido movimiento a partir de una fecha ](http://www.senado.cl/wspublico/invoca_tramitacion_fecha.html). En base a esto lo que hacemos es revisar cada uno y crear una votación en local y a la vez ir a buscarlo a la cámara.
    
    Acá tengo una duda. Este servicio me muestra aquellos proyectos que tuvieron votaciones en la cámara de diputados también?

--- lib/scrapers/motion_scrapper.rb
+++ lib/scrapers/motion_scrapper.rb
@@ -3,83 +3,121 @@ require 'nokogiri'
 require 'htmlentities'
 require 'open-uri'
 
-
 module CongresoAbiertoScrapers
-	class MotionScrapingRunRecord
-		include Mongoid::Document
-		field :date, type: Date
-	end
+    class MotionScrapingRunRecord
+        include Mongoid::Document
+        field :date, type: Date
+    end
 
-	class MotionScraper < Pupa::Processor
-		def dater
-			if @options.include? :dater
-				return @options[:dater]
-			end
-			return Date
-		end
+    class MotionScraper < Pupa::Processor
+        def dater
+            if @options.include? :dater
+                return @options[:dater]
+            end
+            return Date
+        end
 
-		def scrape_motion
-			previous_records = MotionScrapingRunRecord.order_by(
-				date: 'asc'
-				)
-			if previous_records.count > 0
-				date_encoded = previous_records.last.date.strftime('%d/%m/%Y')
-			else
-				date_encoded = (self.dater.today.months_ago(1)).strftime('%d/%m/%Y')
-			end
-			
-			record = MotionScrapingRunRecord.new date:self.dater.today
-			record.save()
-			url = 'http://www.senado.cl/wspublico/tramitacion.php?fecha='+URI::encode(date_encoded)
-			doc = open(url).read
-			xml_doc  = Nokogiri::XML doc
-			persons_without_votes = []
-			xml_doc.css('proyecto').each do |proyecto_|
-				boletin = proyecto_.css('descripcion/boletin').first.content.strip
-				proyecto_.css('votaciones').each do |votaciones|
-					votaciones.css('votacion').each do |votacion|
-						motion = Popolo::Motion.new
-						motion.date = self.dater.strptime votacion.css('FECHA').first.content, '%d/%m/%Y'
-						motion.identifier = boletin
-						motion.text = votacion.css('TEMA').first.content.strip
-						motion.save()
-						vote_event = Popolo::VoteEvent.new
-						vote_event.motion = motion
-						vote_event.save()
-						votacion.css('VOTO').each do |voto|
-							parlamentario_name = voto.css('PARLAMENTARIO').first.content.strip
-							persons = Popolo::Person.where(
-								"other_names.name"=>parlamentario_name
-								)
-							parlamentario_seleccion = voto.css('SELECCION').first.content
-							
-							if persons.count > 0
-								voto = Popolo::Vote.new
-								voto.vote_event = vote_event
-								voto.option = parlamentario_seleccion
-								
-								person = persons.first
-								person.votes.push voto
-								voto.save()
-								person.save()
-							else
-								if persons_without_votes.include? parlamentario_name
-									persons_without_votes.push(parlamentario_name)
-								end
-								
-							end
-						end
-					end
-				end
-			end
-			puts "People that we could not find"
-			persons_without_votes.each do |parlamentario_name|
-				puts parlamentario_name
-			end
+        def scrape_motion
+            previous_records = MotionScrapingRunRecord.order_by(
+                date: 'asc'
+                )
+            if previous_records.count > 0
+                previous_record = previous_records.last.date
+                date_encoded = previous_record.strftime('%d/%m/%Y')
+            else
+                previous_record = self.dater.today.months_ago(1)
+                date_encoded = (previous_record).strftime('%d/%m/%Y')
+            end
+            
+            record = MotionScrapingRunRecord.new date:self.dater.today
+            record.save()
+            url = 'http://www.senado.cl/wspublico/tramitacion.php?fecha='+URI::encode(date_encoded)
+            doc = open(url).read
+            xml_doc  = Nokogiri::XML doc
+            persons_without_votes = []
+            xml_doc.css('proyecto').each do |proyecto_|
+                boletin = proyecto_.css('descripcion/boletin').first.content.strip
+                
+                proyecto_.css('votaciones').each do |votaciones|
+                    votaciones.css('votacion').each do |votacion|
+                        motion = Popolo::Motion.new
+                        motion.date = self.dater.strptime votacion.css('FECHA').first.content, '%d/%m/%Y'
+                        motion.identifier = boletin
+                        motion.text = votacion.css('TEMA').first.content.strip
+                        motion.save()
+                        vote_event = Popolo::VoteEvent.new
+                        vote_event.motion = motion
+                        vote_event.save()
+
+                        get_votaciones_diputados_por_boletin(boletin, vote_event, previous_record)
+
+                        votacion.css('VOTO').each do |voto|
+                            parlamentario_name = voto.css('PARLAMENTARIO').first.content.strip
+                            persons = Popolo::Person.where(
+                                "other_names.name"=>parlamentario_name
+                                )
+                            parlamentario_seleccion = voto.css('SELECCION').first.content
+                            
+                            if persons.count > 0
+                                voto = Popolo::Vote.new
+                                voto.vote_event = vote_event
+                                voto.option = parlamentario_seleccion
+                                
+                                person = persons.first
+                                person.votes.push voto
+                                voto.save()
+                                person.save()
+                            else
+                                if persons_without_votes.include? parlamentario_name
+                                    persons_without_votes.push(parlamentario_name)
+                                end
+                                
+                            end
+                        end
+                    end
+                end
+            end
+            puts "People that we could not find"
+            persons_without_votes.each do |parlamentario_name|
+                puts parlamentario_name
+            end
 
 
-		end
-	end
+        end
+
+        def get_votaciones_diputados_por_boletin(boletin, vote_event, previous_record)
+            url = 'http://opendata.congreso.cl/wscamaradiputados.asmx/getVotaciones_Boletin?prmBoletin='+URI::encode(boletin)
+            doc = open(url).read
+
+            xml_doc  = Nokogiri::XML doc
+            xml_doc.css('Votaciones/Votacion').each do |votacion|
+                date = Date.strptime votacion.css('Fecha').first.content.strip
+                if date >= previous_record
+                    votacion_id = votacion.css('ID').first.content.strip
+
+                    url2 = 'http://opendata.congreso.cl/wscamaradiputados.asmx/getVotacion_Detalle?prmVotacionID='+URI::encode(votacion_id)
+                    doc2 = open(url2).read
+                    xml_doc2  = Nokogiri::XML doc2
+                    xml_doc2.css('Votacion/Votos/Voto').each do |voto|
+                        nombre = voto.css('Nombre').first.content.strip
+                        apellido_paterno = voto.css('Apellido_Paterno').first.content.strip
+                        apellido_materno = voto.css('Apellido_Materno').first.content.strip
+                        nombre_completo = nombre + " " + apellido_paterno + " " + apellido_materno
+                        if Popolo::Person.where(name: nombre_completo).count > 0
+                            person = Popolo::Person.where(name: nombre_completo).first
+                            voto = Popolo::Vote.new
+                            voto.vote_event = vote_event
+                            voto.save
+                            person.votes.push voto
+
+                        end
+
+                    end
+
+                end
+            end
+        end
+    end
 end
 CongresoAbiertoScrapers::MotionScraper.add_scraping_task(:motion)
 
--- spec/pupa_scrapers/motion_scraper_spec.rb
+++ spec/pupa_scrapers/motion_scraper_spec.rb
@@ -6,12 +6,26 @@ require './lib/scrapers/motion_scrapper'
 
 
 WebMock.disable_net_connect!(:allow => /popoloproject.com/)
-
+# WebMock.disable_net_connect!(:allow => /opendata.congreso.cl/)
+possible_bill_ids = ["4162-07","4426-07","4599-07","5491-24","5857-13","6110-24","6201-02","6422-07","6499-11","6556-10","6562-07","6829-01","7011-07","7130-07","7523-24","7550-06","7765-07","7873-07","7908-15","8026-11","8049-17","8069-14","8132-26","8182-10","8207-07","8239-07","8263-13","8307-15","8335-24","8480-03","8624-07","8668-04","8784-04","8805-07","8810-07","8859-04","8886-11","8980-06","9057-04","9097-21","9099-24","9109-02","9126-13","9151-21","9252-15","9276-07","9287-06","9326-07","9333-04","9336-25","9337-16","9365-04","9366-04","9372-07","9386-13","9398-04","9402-13","9404-12","9405-04","9407-14","9430-06","9464-10","9481-04","9484-15","9507-06","9514-07","9515-08","9529-07","9557-04","9566-29","9596-06","9597-07","9608-07","9624-08","9628-08","9669-07","9683-12","9690-01","9691-06","9692-07","9707-02","9715-07","9766-04","9777-06","9780-07","9781-16","9782-16","9783-14","9784-07","9785-14","9786-07","9787-07","9788-07","9789-13","9790-07","9791-05","9792-03","9793-05","9794-06","9795-07","9796-07","9797-07","9798-11","9799-12","9800-12","9801-15","9802-12","9803-03","9804-03","9805-04","9806-06","9807-07","9808-07","9809-17","9810-01","9811-03","9812-03","9813-07","9814-11","9815-15","9816-15","9817-15","9818-17","9819-24","9820-07","9821-03","9822-07","9823-07","9824-17"]
+possible_vote_event_id = ["20456","20455","20467","20459","20451","20453","20452","20454","20458","20457","20461","20466","20462","20468","20460"]
 
 describe CongresoAbiertoScrapers::MotionScraper , "The Motion Scrapper" do
 	before :each do
 		$file = File.read('./spec/fixtures/ultimas_tramitaciones.xml')
-		
+		stub_request(:get, "http://opendata.congreso.cl/wscamaradiputados.asmx/getVotaciones_Boletin?prmBoletin=4162-07")
+			.to_return(:body => "<Votaciones />")
+
+		possible_bill_ids.each do |bill_id|
+			get_votaciones_boletin3 = File.read('./spec/fixtures/votaciones_camara_boletin/'+bill_id+'.xml')
+			stub_request(:get, "http://opendata.congreso.cl/wscamaradiputados.asmx/getVotaciones_Boletin?prmBoletin="+bill_id)
+				.to_return(:body => get_votaciones_boletin3)
+		end
+		possible_vote_event_id.each do |vote_event_id|
+			get_votaciones_boletin3 = File.read('./spec/fixtures/votaciones_camara_boletin/votacion_'+vote_event_id+'.xml')
+			stub_request(:get, "http://opendata.congreso.cl/wscamaradiputados.asmx/getVotacion_Detalle?prmVotacionID="+vote_event_id)
+				.to_return(:body => get_votaciones_boletin3)
+		end
 	end
 	
 	context "it self" do
@@ -45,7 +59,8 @@ describe CongresoAbiertoScrapers::MotionScraper , "The Motion Scrapper" do
 		end
 
 		it "records every run" do
-
+			stub_request(:get, /www.senado.cl/)
+			stub_request(:get, /opendata.congreso.cl/)
 			Date.stub(:today).and_return(today)
 			runner = Pupa::Runner.new(CongresoAbiertoScrapers::MotionScraper)
 			runner.run([:level => "ERROR"])
@@ -174,6 +189,22 @@ describe CongresoAbiertoScrapers::MotionScraper , "The Motion Scrapper" do
 			expect(voto).to_not be_nil
 			expect(voto.voter._id).to eq(person.id)
 			expect(voto.voter.id).to eq(person.id)
+		end
+		it "va a la camara a buscar cosas" do
+			person = Popolo::Person.create name:"Marco Antonio Núñez Lozano", id:"5330375fd0c05d8b737b6d61"
+			person.save
+
+			runner = Pupa::Runner.new(CongresoAbiertoScrapers::MotionScraper)
+			runner.run([:level => "ERROR"])
+
+			motion = Popolo::Motion.where(
+				text:"Aprobación en general del proyecto de ley, en segundo trámite constitucional, que modifica la ley N° 17.798, de Control de Armas, y el Código Procesal Penal, con informe de la Comisión de Defensa Nacional (Boletín N° 6.201-02).").first
+
+			vote_event = motion.vote_events.first
+			votos = vote_event.votes.where(
+				voter:person)
+			expect(votos.count).to be > 0
+			expect(votos.first.voter.id).to eq(person.id)
 		end
 	end
 
